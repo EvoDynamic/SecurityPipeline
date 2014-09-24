82c479e5641ed50656606c90e1ebda871f559341
BrianSeim <bseim@uncommonsolutions.com>
2014-09-24 13:52:12 -0600

    Security pipeline HttpModule > Middleware > AuthenticationFilter > AuthorizationFilter > Controller.

---
+++ SecurityPipeline/Pipeline/TestMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SecurityPipeline.Pipeline
+{
+	public class TestMiddleware
+	{
+		private Func<IDictionary<string,object>, Task> _next;
+		public TestMiddleware(Func<IDictionary<string,object>, Task> next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(IDictionary<string, object> env)
+		{
+			var context = new OwinContext(env);
+
+			//authenticate Principal: this could be done at any stage of the pipeline.
+			context.Request.User = 
+				new GenericPrincipal(new GenericIdentity("bri"), new string[] { });
+
+			Helper.Write("Middleware", context.Request.User);
+
+			await _next(env);
+		}
+	}
+}
---
+++ SecurityPipeline/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Owin;
+using SecurityPipeline.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SecurityPipeline
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			//this is the new global.ascx
+			var configuraiton = new HttpConfiguration();
+			configuraiton.Routes.MapHttpRoute(
+				"default",
+				"api/{controller}");
+
+			//to add this globally
+			//configuraiton.Filters.Add(TestAuthenticationFilterAttribute f)
+			//otherwise use it on the controller itself.
+
+			app.Use(typeof(TestMiddleware));
+
+			app.UseWebApi(configuraiton);
+		}
+	}
+}
